8a71de858cc9265041de08059fd1f90b88e28057
Sandy <65991137+SandyLuXY@users.noreply.github.com>
2020-07-22 21:50:18 +0800

    Add files via upload

--- controller_code.m
+++ controller_code.m
@@ -1,20 +1,20 @@
 function [exectime, data] = controller_code(seg, data)
+
 switch seg
-case 1
- y = ttGetMsg; % Obtain sensor value
-if isempty(y)
- disp('Error in controller: no message received!');
- y = 0.0;
-end
- r = ttAnalogIn(1); % Read reference value
- data.MPn = data.K * (r - y);
- data.MIn = data.K * data.h/data.Ti * (r - y) + data.MX;
- data.MDn = data.K * data.Td/data.h * (data.yold - y);
- data.u = data.MPn + data.MIn + data.MDn;
- data.MX = data.MIn;
- data.yold = y;
- exectime = 0.0005;
-case 2
- ttSendMsg(2, data.u, 100); % Send the value to the actuator
- exectime = -1; % finished
+ case 1
+  y = ttGetMsg;         % Obtain sensor value
+  if isempty(y)
+    disp('Error in controller: no message received!');
+    y = 0.0;
+  end
+  r = ttAnalogIn(1);    % Read reference value
+  P = data.K*(r-y);
+  D = data.ad*data.Dold + data.bd*(data.yold-y);
+  data.u = P + D;
+  data.Dold = D;
+  data.yold = y;
+  exectime = 0.0005;
+ case 2
+  ttSendMsg(2, data.u, 80);    % Send 80 bits to node 2 (actuator)
+  exectime = -1; % finished
 end
