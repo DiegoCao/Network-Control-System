a8502ea36294922f926185c002acaf8eb60ca621
ZhimanSu <68639262+ZhimanSu@users.noreply.github.com>
2020-07-24 11:47:51 +0800

    Add files via upload

---
+++ node_code_szm.m
@@ -0,0 +1,20 @@
+function [exectime, data] = node_code(seg, data) 
+switch seg 
+    case 1   
+        y = ttGetMsg;        % Obtain sensor value    
+        if isempty(y)     
+            disp('Error in controller: no message received!');    
+            y = 0.0; 
+        end
+        r = ttAnalogIn(1);   % Read reference value   
+        data.MPn = data.K * (r - y);   
+        data.MIn = data.K * data.h/data.Ti * (r - y) + data.MX;  
+        data.MDn = data.K * data.Td/data.h * (data.yold - y);   
+        data.u = data.MPn + data.MIn + data.MDn;   
+        data.MX = data.MIn;   
+        data.yold = y;   
+        exectime = 0.0005;  
+    case 2   
+        ttSendMsg(2, data.u, 100);  % Send the value to the actuator  
+        exectime = -1;              % finished
+end
